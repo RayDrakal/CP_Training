efa7bf395f285a216240900559530efde840e2d7
RayDrakal <31558022+RayDrakal@users.noreply.github.com>
2025-04-22 05:51:20 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define INF 0x3f3f3f3f
+using namespace std;
+int main() {
+	int i;
+	string s;
+	cin >> s >> i;
+	cout << s[i-1];
+}
