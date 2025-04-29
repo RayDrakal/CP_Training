b20255b327f9ab0e4526984547b7678622330256
RayDrakal <31558022+RayDrakal@users.noreply.github.com>
2025-04-29 17:57:30 +0900

    [Gold I] Title: Balanced Lineup, Time: 176 ms, Memory: 5156 KB -BaekjoonHub

---
+++ 백준/Gold/6213. Balanced Lineup/Balanced Lineup.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define INF 0x3f3f3f3f
+using namespace std;
+typedef long long ll;
+const ll mod = 1e9 + 7;
+ll n, k;
+class segment {
+public:
+	vector<ll> seg;
+	int n;
+	segment(int n) {
+		seg.resize(4 * n + 5);
+		this->n = n;
+	}
+	ll update(int node, ll l, ll r, ll tg, ll val) {
+		if (tg<l || tg>r) return seg[node];
+		if (tg == l && tg == r) return seg[node] = val;
+		ll mid = (l + r) / 2;
+		return seg[node] = min(update(node * 2, l, mid, tg, val),
+			update(node * 2 + 1, mid + 1, r, tg, val));
+	}
+	ll mupdate(int node, ll l, ll r, ll tg, ll val) {
+		if (tg<l || tg>r) return seg[node];
+		if (tg == l && tg == r) return seg[node] = val;
+		ll mid = (l + r) / 2;
+		return seg[node] = max(mupdate(node * 2, l, mid, tg, val),
+			mupdate(node * 2 + 1, mid + 1, r, tg, val));
+	}
+	ll qer(int node, ll l, ll r, ll xl, ll xr) {
+		if (l > xr || r < xl) return 1e6 + 1;
+		if (xl <= l && r <= xr) return seg[node];
+		ll mid = (l + r) / 2;
+		return min(qer(node * 2, l, mid, xl, xr),
+			qer(node * 2 + 1, mid + 1, r, xl, xr));
+	}
+	ll mqer(int node, ll l, ll r, ll xl, ll xr) {
+		if (l > xr || r < xl) return 0;
+		if (xl <= l && r <= xr) return seg[node];
+		ll mid = (l + r) / 2;
+		return max(mqer(node * 2, l, mid, xl, xr),
+			mqer(node * 2 + 1, mid + 1, r, xl, xr));
+	}
+};
+int main() {
+	FASTIO;
+	cin >> n >> k;
+	segment s(n + 1);
+	segment S(n + 1);
+	for (int i = 0; i < n; i++) {
+		ll a; cin >> a;
+		S.mupdate(1, 0, n - 1, i, a);
+		s.update(1, 0, n - 1, i, a);
+	}
+	while (k--) {
+		ll a, b;
+		cin >> a >> b;
+		a--, b--;
+		cout << S.mqer(1, 0, n - 1, a, b) - s.qer(1, 0, n - 1, a, b) << '\n';
+	}
+}
