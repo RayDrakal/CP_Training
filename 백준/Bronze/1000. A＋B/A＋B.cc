58fc5b9ff1e0e6b434683aa4a5c63dc34115da10
RayDrakal <31558022+RayDrakal@users.noreply.github.com>
2025-05-10 17:23:00 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define INF 0x3f3f3f3f
+using namespace std;
+
+int main() {
+	FASTIO;
+	int A, B;
+	cin >> A >> B;
+	cout << A + B;
+}
